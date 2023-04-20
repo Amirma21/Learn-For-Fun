a0badcab3e32b16c56747de125d764cb197644d9
Amir maleki <amirmaleki21physics@gmail.com>
2023-04-20 19:58:04 +0330

    manually implement the forEach method.

---
+++ src/jsBuiltInFunctiones/forEeach.js
@@ -0,0 +1,29 @@
+/// In this file, I want to manually implement the forEach method.
+
+const numbers = [1, 3, 5, 7, 8, 9]
+
+function forEach(array, callBackFn) {
+    for (let i = 0; i < array.length; i++) {
+        callBackFn(array[i], i, array)
+    }
+}
+
+
+/// outPut :
+// number in index 0 is 1
+// number in index 1 is 3
+// number in index 2 is 5
+// number in index 3 is 7
+// number in index 4 is 8
+// number in index 5 is 9
+
+
+forEach(numbers , (num , index)=>{
+    console.log(`number in index ${index} is ${num}`)
+})
+
+
+
+// numbers.forEach((value , index)=>{
+//     console.log(index)
+// })
